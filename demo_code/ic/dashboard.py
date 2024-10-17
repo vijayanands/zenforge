fff0cb2f0bb40877609f624bd7f0895d37733ab2
Vijay Sankarasubramanian <vijayanands@gmail.com>
2024-10-16 17:19:36 -0700

    did isort

--- demo_code/ic/dashboard.py
+++ demo_code/ic/dashboard.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
-from demo_code.ic.ic_performance_and_careeer_dashboard import (
-    ic_perf_and_career_dashboard,
-)
+from demo_code.ic.ic_performance_and_careeer_dashboard import \
+    ic_perf_and_career_dashboard
 from demo_code.ic.ic_productivity_dashboard import ic_productivity_dashboard
 
 
